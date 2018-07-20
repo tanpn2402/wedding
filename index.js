77fa77ff58c55588978e9a5b489c85ec06850dd6
tanpn2402 <tanpn2402@gmail.com>
2018-07-20 22:54:18 +0700

    ad

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express'),
+    app = express(),
+    template = require('./build/template')
+    path = require('path'),
+    ssr = require('./build/server'),
+
+    
+// Serving static files
+app.use('/assets/styles', express.static(path.resolve(__dirname, 'assets/styles')));
+app.use('/assets/js', express.static(path.resolve(__dirname, 'assets/js')));
+app.use('/assets/images', express.static(path.resolve(__dirname, 'assets/images')));
+
+
+// hide powered by express
+app.disable('x-powered-by');
+// start the server
+app.listen(process.env.PORT || 8000)
+
+app.get('/', ( req , res) => {
+    const { preloadedState, content}  = ssr()
+    const response = template("Home", content, preloadedState)
+    res.setHeader('Cache-Control', 'assets, max-age=604800')
+    res.send(response);
+});
